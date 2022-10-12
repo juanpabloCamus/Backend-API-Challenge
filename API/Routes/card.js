deeba1851eb67863ccdb51178b34994ca4074712
Juan Pablo <juanpablocamus28@gmail.com>
2022-10-12 15:14:54 -0300

    adding get all cards endpoint

--- API/Routes/card.js
+++ API/Routes/card.js
@@ -1,7 +1,14 @@
 const cardRouter = require('express').Router();
+const Card = require('../Models/Card');
 
-cardRouter.get('/', (req, res) => {
-  res.send('card');
+cardRouter.get('/', async (req, res) => {
+  try {
+    const cards = await Card.find();
+    if (cards.length === 0) return res.send('No cards created yet!');
+    res.send(cards);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = cardRouter;
