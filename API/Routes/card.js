f317dc1bbdb6cd1a80548cbdcfcfbaf05fbb578f
Juan Pablo <juanpablocamus28@gmail.com>
2022-10-12 15:32:45 -0300

    adding find by name endpoint

--- API/Routes/card.js
+++ API/Routes/card.js
@@ -5,7 +5,18 @@ cardRouter.get('/', async (req, res) => {
   try {
     const cards = await Card.find();
     if (cards.length === 0) return res.send('No cards created yet!');
-    res.send(cards);
+    return res.send(cards);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+cardRouter.get('/:name', async (req, res) => {
+  try {
+    const { name } = req.params;
+    const cards = await Card.findOne({ name });
+    if (cards === null) return res.send('No card found with that name');
+    return res.send(cards);
   } catch (error) {
     console.log(error);
   }
